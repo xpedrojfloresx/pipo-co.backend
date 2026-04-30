758a097a14f85c6a617805861cecf65d4dbeae62
Pedro Flores <pflores0213@gmail.com>
2026-04-30 13:58:32 -0300

    fixign db uri

--- database/conexionAtlas.js
+++ database/conexionAtlas.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}`;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
 const clientOptions = {
     serverApi: {
